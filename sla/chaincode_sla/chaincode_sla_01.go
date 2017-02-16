abc71d6e14b53ae157bd848b700e39fb3cf7dbb7
SeungPhilHeo <namjun719@naver.com>
2017-02-16 17:20:50 +0900

    ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ

--- sla/chaincode_sla/chaincode_sla_01.go
+++ sla/chaincode_sla/chaincode_sla_01.go
@@ -1,4 +1,4 @@
-/*copyright IBM Corp. 2016 All Rights Reserved.(test_20170216)
+/*copyright IBM Corp. 2016 All Rights Reserved.(test_20170216¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
