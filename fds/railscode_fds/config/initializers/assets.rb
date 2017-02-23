609c4dc59cb4bf5ded747e745c07ee4bf58a575f
chae9214 <chae9214@gmail.com>
2017-02-23 18:02:26 +0900

    Set css paths and migrate mall webpage to rails

--- fds/railscode_fds/config/initializers/assets.rb
+++ fds/railscode_fds/config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( fds.css )
+Rails.application.config.assets.precompile += %w( bank.css )
+Rails.application.config.assets.precompile += %w( mall/style.css )
+Rails.application.config.assets.precompile += %w( mall/normalize.css )
+Rails.application.config.assets.precompile += %w( mall/bootstrap.min.css )
--- fds/railscode_fds/config/routes.rb
+++ fds/railscode_fds/config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   # match 'tra', to: 'tra#index', via: :all
 
   match 'transfer', to: 'transfer#trans', via: :all
-  resources :malls
   resources :fdsbanks
   resources :transactions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
